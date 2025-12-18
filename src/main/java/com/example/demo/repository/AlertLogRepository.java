9b5ff75eb701789f82ef38ad0d214a479eadde27
bala-amypo <bala@amypo.in>
2025-12-18 14:46:36 +0000

    User commit

--- src/main/java/com/example/demo/repository/AlertLogRepository.java
+++ src/main/java/com/example/demo/repository/AlertLogRepository.java
@@ -2,7 +2,7 @@ package com.example.demo.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface AlertLogRepository extends JpaRepository{
+public interface AlertLogRepository extends JpaRepository<>{
 
 
 
