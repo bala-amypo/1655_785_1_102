07fe1ee9b2a78c3c5e05456695d5ddb447e7ad53
bala-amypo <bala@amypo.in>
2025-12-18 15:33:18 +0000

    User commit

--- src/main/java/com/example/demo/service/AlertScheduleService.java
+++ src/main/java/com/example/demo/service/AlertScheduleService.java
@@ -2,5 +2,7 @@ package com.example.demo.service;
 
 public interface AlertScheduleService{
 
+AlertSchedule createSchedule(Long warrantyId,AlertSchedule schedule);
+List<AlertSchedule> getLogs(Long warrantyId);
     
 }
--- src/main/java/com/example/demo/service/WarrantyService.java
+++ src/main/java/com/example/demo/service/WarrantyService.java
@@ -1,5 +1,6 @@
 package com.example.demo.service;
-
+import com.example.demo.entity.Warranty;
+import java.util.List;
 public interface WarrantyService{
 
     Warranty registerWarranty(Long userId,Long productId,Warranty warranty);
