7181480d96849a9c527df93e5599d11da6cb1957
bala-amypo <bala@amypo.in>
2025-12-18 13:36:43 +0000

    User commit

--- src/main/java/com/example/demo/entity/Product.java
+++ src/main/java/com/example/demo/entity/Product.java
@@ -1,5 +1,9 @@
 package com.example.demo.entity;
-
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.validation.constraints.Column;
 public class Product{
     
 }
