a147e1000b43317d63b6d6156f1cca01c1369619
bala-amypo <bala@amypo.in>
2025-12-18 17:38:21 +0000

    User commit

--- src/main/java/com/example/demo/entity/Warranty.java
+++ src/main/java/com/example/demo/entity/Warranty.java
@@ -21,21 +21,46 @@ public class Warranty{
     private String serialNumber;
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+public String getName(){
+        return name;
+    }
+    public void setName(String name){
+        this.name=name;
+    }
     
+    public Long getId(){
+        return id;
+    }
+    public void setId(Long id){
+        this.id=id;
+    }
+
+    public String getBrand(){
+        return brand;
+    }
+    public void setBrand(String brand){
+        this.brand=brand;
+    }
+    public String getmodelNumber(){
+        return modelNumber;
+    }
+    public void setmodelNumber(String modelNumber){
+        this.modelNumber=modelNumber;
+    }
+    public String getcategory(){
+        return category;
+    }
+    public void setcategory(String category){
+        this.category=category;
+    }
+
+  public Warranty(String name,Long id,String brand,String modelNumber,String category){
+        this.name=name;
+        this.id=id;
+        this.brand=brand;
+        this.modelNumber=modelNumber;
+        this.category=category;
+    }
+    public Warranty(){
+    }
 }
