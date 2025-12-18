63cef932684bd9717286e4e4fa68845cc5bb8808
bala-amypo <bala@amypo.in>
2025-12-18 15:15:19 +0000

    User commit

--- src/main/java/com/example/demo/entity/AlertLog.java
+++ src/main/java/com/example/demo/entity/AlertLog.java
@@ -1,5 +1,19 @@
 package com.example.demo.entity;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.ManyToOne;
+
+@Entity
 public class AlertLog{
     
+    @Id
+    @GeneratedValue(strategy=GenerationType.IDENTITY)
+    private Long id;
+    @ManyToOne
+    private Warranty warranty;
+    private ;
+    private String message;
 }
--- src/main/java/com/example/demo/entity/AlertSchedule.java
+++ src/main/java/com/example/demo/entity/AlertSchedule.java
@@ -3,7 +3,7 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.Id;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.;
+import jakarta.persistence.ManyToOne;
 
 @Entity
 public class AlertSchedule{
