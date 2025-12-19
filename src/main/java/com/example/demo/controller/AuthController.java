b32a56f981160de04c3db29a37843c3f9139b0c1
bala-amypo <bala@amypo.in>
2025-12-19 16:26:21 +0000

    User commit

--- src/main/java/com/example/demo/controller/AuthController.java
+++ src/main/java/com/example/demo/controller/AuthController.java
@@ -5,6 +5,7 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import com.example.demo.service.UserService;
 import com.example.demo.entity.User;
+import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class AuthController{
    @Autowired UserService us;
--- src/main/java/com/example/demo/repository/WarrantyRepository.java
+++ src/main/java/com/example/demo/repository/WarrantyRepository.java
@@ -1,8 +1,8 @@
 package com.example.demo.repository;
-
+import com.example.demo.entity.Warranty;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface WarrantyRepository extends JpaRepository<AlertLog,Long>{
+public interface WarrantyRepository extends JpaRepository<Warranty,Long>{
 
 
     
