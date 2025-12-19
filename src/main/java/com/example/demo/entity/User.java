0ebbec46440e0455a122bf2eacd1055135bec874
bala-amypo <bala@amypo.in>
2025-12-19 14:50:24 +0000

    User commit

--- src/main/java/com/example/demo/entity/User.java
+++ src/main/java/com/example/demo/entity/User.java
@@ -4,7 +4,12 @@ import jakarta.persistence.Id;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.validation.constraints.Column;
+import
+
 @Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class User{
     @Id
     @GeneratedValue(strategy=GenerationType.IDENTITY)
@@ -15,46 +20,4 @@ public class User{
     private String password;
     private String role;
 
-    public String getName(){
-        return name;
-    }
-    public void setName(String name){
-        this.name=name;
-    }
-
-    public Long getId(){
-        return id;
-    }
-    public void setId(Long id){
-        this.id=id;
-    }
-
-    public String getEmail(){
-        return email;
-    }
-    public void setEmail(String email){
-        this.email=email;
-    }
-    public String getPassword(){
-        return password;
-    }
-    public void setPassword(String password){
-        this.password=password;
-    }
-    public String getRole(){
-        return role;
-    }
-    public void setRole(String role){
-        this.role=role;
-    }
-
-  public User(String name,Long id,String email,String password,String role){
-        this.name=name;
-        this.id=id;
-        this.email=email;
-        this.password=password;
-        this.role=role;
-    }
-    public User(){
-    }
 }
